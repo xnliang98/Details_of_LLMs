b4f6b0a33e545ac3a60cd17095a466c1a8703f22
xnliang <xnliang19@163.com>
2023-09-26 15:35:28 +0800

    add json line io

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import time
+import json
 from functools import wraps
 
 
@@ -9,6 +10,19 @@ def time_it(func):
         result = func(*args, **kw)
         end_time = time.time()
         print("函数的运行时间(s)：{}".format(end_time - start_time))
-        print("函数的运行时间(ms)：{}".format(float(end_time - start_time) * 1000.0))
+        # print("函数的运行时间(ms)：{}".format(float(end_time - start_time) * 1000.0))
         return result
     return wrapper
+
+@time_it
+def read_json_lines(file_name):
+    with open(file_name, 'r') as file:
+        data = [json.loads(line) for line in file]
+    return data
+
+@time_it
+def save_json_lines(data, file_name):
+    with open(file_name, 'w') as file:
+        for item in data:
+            json.dump(item, file)
+            file.write('\n')
